a7d5a82ef1ae4134b4be923415705a877145b1a8
Kim Miseon <o_oa98@naver.com>
2021-05-14 14:09:37 +0900

    수정

--- pybo/models.py
+++ pybo/models.py
@@ -9,12 +9,12 @@ class Question(db.Model):
 class Answer(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     question_id=db.Column(db.Integer,db.ForeignKey('question.id',ondelete='CASCADE'))
-    question=db.relationship('Question',backref=db.backref('answer_set')) #answer_set으로 자동으로 데이터 들어감
+    question=db.relationship('Question',backref=db.backref('answer_set'))
     content=db.Column(db.Text(),nullable=False)
     create_date=db.Column(db.DateTime,nullable=False)
 
 class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(150),unique=True,nullable=False)  #유니크:같은값 저장x
+    username=db.Column(db.String(150),unique=True,nullable=False)
     password=db.Column(db.String(200),nullable=False)
     email=db.Column(db.String(120),unique=True,nullable=False)
