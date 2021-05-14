613e9fa6adc72d9c45d2b5bdebceb7f1a07d413c
Kim Miseon <o_oa98@naver.com>
2021-05-14 14:05:39 +0900

    수정

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
 
-BASE_DIR=os.path.dirname(__file__)  # 현재 폴더경로를 가져옴
+BASE_DIR=os.path.dirname(__file__)
 
-# format(문구) -> {}에 들어갈 문구
-# os.path.join(경로,데이터베이스) -> 경로에 있는 데이터베이스 사용
 SQLALCHEMY_DATABASE_URI='sqlite:///{}'.format(os.path.join(BASE_DIR,'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 
