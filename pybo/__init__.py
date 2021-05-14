6a149233e9bfb6631e4d895d52c12ff0d30b91c8
Kim Miseon <o_oa98@naver.com>
2021-05-14 14:11:16 +0900

    수정

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -15,14 +15,13 @@ def create_app():
 
     from . import models
 
-    from .views import main_views,naver_views,question_views,auth_views,answer_views,naver_views,movie_views
+    from .views import main_views,naver_views,question_views,auth_views,answer_views,movie_views
 
     app.register_blueprint(main_views.bp)
     app.register_blueprint(naver_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
     app.register_blueprint(auth_views.bp)
-    app.register_blueprint(naver_views.bp)
     app.register_blueprint(movie_views.bp)
 
     from .filter import format_datetime
